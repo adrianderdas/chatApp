6e874a3de16fc54d11c337d9bf4878038daabfaf
adrianderdas <derdas.adrian@gmail.com>
2023-05-04 03:47:46 +0200

    Added working sending messages to firebase

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -12,6 +12,8 @@ import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
     
+ 
+    
     private let spinner = JGProgressHUD(style: .dark)
     
     
@@ -46,9 +48,27 @@ class ConversationsViewController: UIViewController {
     
     @objc private func didTapComposeButton() {
         let vc = NewConversationViewController()
+        vc.completion = { [weak self] result in
+            self?.createNewConversation(result: result)
+            
+        }
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC, animated: true)
     }
+    
+    private func createNewConversation(result: [String: String]) {
+        guard let name = result["name"],
+            let email = result["email"] else {
+            return
+        }
+        let vc = ChatViewController(with: email)
+        vc.isNewConversation = true
+        vc.title = name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    
+    }
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
@@ -100,7 +120,7 @@ extension ConversationsViewController: UITableViewDelegate, UITableViewDataSourc
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = ChatViewController()
+        let vc = ChatViewController(with: "sdsdsad@gdsgsdg.com")
         vc.title = "Jenny Smith"
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
